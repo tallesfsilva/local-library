79edfd7e44e46058959437ca763c6c748ee4eaaa
tallesfsilva <talles.silva@gmail.com>
2024-03-19 13:04:16 -0300

    Local Library

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.get('/cool', function(req, res, next) {
+  res.send("It's not as expected!");
+});
+
+
+module.exports = router;
